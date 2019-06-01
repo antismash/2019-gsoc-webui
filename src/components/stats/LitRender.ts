f4dfc3e50c61ac150b8689b9e7171cc25dd9d4f8
Jeevan <jiwanprakash65@gmail.com>
2019-06-01 12:13:11 +0530

    refactoring

--- src/components/stats/LitRender.ts
+++ src/components/stats/LitRender.ts
@@ -1,27 +1,12 @@
 import { render, html } from 'lit-html';
 
 const LitRender = (base) => class extends base {
-  renderTemplate = () => {
-    const arr = [
-      { key: "server status: ", val: this.status },
-      { key: "Running Jobs: ", val: this.runningJobs },
-      { key: "Queued Jobs", val: this.queuedJobs },
-      { key: "Jobs Processed", val: this.jobsProcessed },
-    ];
-
-   return html`
-      ${arr.map(({ key, val }) => html`<div>${key}: ${val}</div>`)}
-    `;
-
-    // render(statsTemplate, document.getElementById('stats'));
-  }
-
-  invalidate = async () => {
+  invalidate = async (renderTemplate) => {
     if (!this.needsRender) {
       this.needsRender = true;
       await Promise.resolve();
       this.needsRender = false;
-      render(this.renderTemplate(), this.shadowRoot);
+      render(renderTemplate(), this.shadowRoot);
     }
   }
 }
--- src/components/stats/Stats.ts
+++ src/components/stats/Stats.ts
@@ -4,40 +4,56 @@ import api from '../../../utils/api';
 
 class Stats extends LitRender(HTMLElement) {
   status: string;
-  runningJobs: number;
-  queuedJobs: number;
-  jobsProcessed: number;
+  running: number;
+  queue_length: number;
+  total_jobs: number;
   needsRender: boolean;
+
+  setStats = (data) => {
+    const { status, running, queue_length, total_jobs } = data;
+    this.status = status;
+    this.running = running;
+    this.queue_length = queue_length;
+    this.total_jobs = total_jobs;
+  }
 
   constructor() {
     super();
     this.attachShadow({ mode: 'open' });
-    this.status = "fetching status...";
-    this.runningJobs = 0;
-    this.queuedJobs = 0;
-    this.jobsProcessed = 0;
-    this.invalidate();
+    this.setStats({
+      status: "fetching status...",
+      running: 0,
+      queue_length: 0,
+      total_jobs: 0
+    });
+    this.invalidate(this.renderTemplate);
     this.fetchStats();
-  }
-
-  setStats = (data) => {
-    const { status, running, queue_length, total_jobs } = data;
-
-    this.status = status;
-    this.runningJobs = running;
-    this.queuedJobs = queue_length;
-    this.jobsProcessed = total_jobs;
   }
 
   fetchStats = async () => {
     try {
       const { data } =  await api.get('/data');
       this.setStats(data);
-      this.invalidate();
+      this.invalidate(this.renderTemplate);
     } catch (err) {
       this.status = "error fetching data...";
       console.log(err);
     }
+  }
+
+  renderTemplate = () => {
+    const arr = [
+      { key: "server status: ", val: this.status },
+      { key: "Running Jobs: ", val: this.running },
+      { key: "Queued Jobs", val: this.queue_length },
+      { key: "Jobs Processed", val: this.total_jobs },
+    ];
+
+   return html`
+      ${arr.map(({ key, val }) => html`<div>${key}: ${val}</div>`)}
+    `;
+
+    // render(statsTemplate, document.getElementById('stats'));
   }
 }
 
