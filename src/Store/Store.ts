416c1adde900e5574202ff5fffb2f8b41efff842
Jeevan <jiwanprakash65@gmail.com>
2019-06-10 13:29:29 +0530

    refactoring

--- src/Store/Store.ts
+++ src/Store/Store.ts
@@ -1,15 +1,6 @@
 import { createStore } from 'redux';
 
-function counter(state = 0, action) {
-  switch (action.type) {
-    case 'INCREMENT':
-      return state + 1;
-    case 'DECREMENT':
-      return state - 1;
-    default:
-      return state;
-  }
-}
+import counter from '../Reducers/Counter';
 
 let store = createStore(counter);
 export default store;
