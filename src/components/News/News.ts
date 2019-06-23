1cea7aed8be9b2443eae163e94e318828f262cc6
Jeevan <jiwanprakash65@gmail.com>
2019-06-23 16:07:45 +0530

    Updated Design of News component

--- src/components/News/News.ts
+++ src/components/News/News.ts
@@ -11,7 +11,6 @@ class News extends Litrender(HTMLElement) {
 
   setNews(data) {
     this.news = { ...data };
-    console.log(data);
   }
 
   constructor() {
@@ -58,16 +57,40 @@ class News extends Litrender(HTMLElement) {
     }
   }
 
+  hideNews = e => {
+    console.log(e);
+    e.target.parentElement.style.display = "none";
+  }
+
   renderTemplate = () => {
     const { teaser, text } = this.news.notices[0];
 
     return html`
       <style>
+        #cross {
+          opacity: 0.8;
+          float: right;
+          cursor: pointer;
+        }
+        #cross:hover {
+          color: #31708f;
+        }
         #container {
           padding: 1rem;
+          position: absolute;
+          z-index: 10;
+          top: 60px;
+          right: 10px;
+          width: 250px;
+          border: solid 1px #31708f;
+          border-radius: 5px;
+          background: linear-gradient(to bottom, #d9edf7 0, #b9def0 100%);
+          opacity: ${text === 'loading' ? 0 : 1};
+          transition: opacity 2s;
         }
       </style>
       <div id="container">
+        <div id="cross" @click="${e => {e.target.parentElement.style.display = "none";}}">&#10008;</div>
         <h3>${teaser}</h3>
         <p>${text}</p>
       </div>
